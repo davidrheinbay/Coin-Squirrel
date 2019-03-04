bd382a9379fb07ddc37c2a83403beaa0fdcf7a0f
Konrad Baumann <baumannkonrad@outlook.de>
2019-03-04 15:37:35 +0100

    fixes datetime

--- db/migrate/20190304142808_create_exchange_rates.rb
+++ db/migrate/20190304142808_create_exchange_rates.rb
@@ -4,8 +4,8 @@ class CreateExchangeRates < ActiveRecord::Migration[5.2]
       t.string :currency_origin_short
       t.string :currency_target_short
       t.float :rate
-      t.date_time :date_created
-      t.date_time :date_updated
+      t.datetime :date_created
+      t.datetime :date_updated
       t.references :game, foreign_key: true
 
       t.timestamps
