f3c693282bccc9baf80b52a59e2e35dc0c4afa81
Maximilian Wingbermuehle <max@texwork.com>
2019-03-05 13:08:06 +0100

    adds pundit to game#show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 require 'json'
 require 'open-uri'
 
-
 class GamesController < ApplicationController
-
   skip_before_action :authenticate_user!, only: :show
 
   def show
+    @game = Game.find(params[:id])
+    authorize @game
     url = 'https://fortnite-public-api.theapinetwork.com/prod09/store/get'
 
     shop = JSON.parse(open(url).read)
