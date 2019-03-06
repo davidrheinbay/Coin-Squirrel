f6b82cce714605a445f006e82aa48faa1e72de6e
David Rheinbay <david.rheinbay@gmail.com>
2019-03-06 16:05:52 +0100

    Landing page responsive to game clicked

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,25 @@ class PagesController < ApplicationController
 
   def home
     @partners = Partner.all
-    if params[:query].blank?
-      @games = Game.all
+    @game = find_game
+    @games = search_games
+  end
+
+  private
+
+  def find_game
+    if params[:game].blank?
+      Game.find_by_name(params["Fortnite"])
     else
-      @games = Game.where("name ILIKE ?", params[:query])
+      Game.find_by_name(params[:game])
+    end
+  end
+
+  def search_games
+    if params[:query].blank?
+      Game.all
+    else
+      Game.where("name ILIKE ?", params[:query])
     end
   end
 end
