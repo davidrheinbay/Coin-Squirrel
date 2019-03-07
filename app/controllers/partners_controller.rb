9e2b0baff600319533bfb5a4a7057993ece1b592
David Rheinbay <david.rheinbay@gmail.com>
2019-03-07 19:18:59 +0100

    adding partnernew and edit edit still with wrong route

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,45 @@
 class PartnersController < ApplicationController
-
   def index
     @partners = policy_scope(Partner).order(created_at: :desc)
+  end
+
+  def new
+    @partner = Partner.new
+    authorize @partner
+  end
+
+  def create
+    @partner = Partner.new(partner_params)
+    authorize @partner
+    if @partner.save
+      redirect_to partners_path(partner: @partner.name)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @partner = Partner.find(params[:id])
+    authorize @partner
+  end
+
+  def update
+    @partner = Partner.find(params[:id])
+    authorize @partner
+    @partner.update(partner_parmas)
+    if @partner.save
+      redirect_to partners_path(partner: @partner.name)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def partner_params
+    params.require(:partner).permit(:name, :company_entity_name,
+                                    :comission_perc, :user_commsission_perc,
+                                    :referral_link, :logo_image, :card_image,
+                                    :awin_advertiser_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :partners, only: [:index]
+  resources :partners, only: [:index, :new, :create, :edit, :update]
   resources :games, only: [:index, :show]
   get "/858d1fa2cc7e0388776f1759981a0d06.html", to: "pages#858d1fa2cc7e0388776f1759981a0d06.html"
   resources :transactions, only: [:index]
