827058f6fd95cc6d09ffc956c1aa894a7c0e900a
Konrad Baumann <baumannkonrad@outlook.de>
2019-03-11 16:15:45 +0100

    adds greyscale to locked items

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import "transition";
 import { previewImageOnFileSelect } from 'photo-upload-preview/photo_upload_preview';
+import { cardUnlocked } from 'packs/store';
 
 previewImageOnFileSelect();
+cardUnlocked();
---
+++ app/javascript/packs/store.js
@@ -0,0 +1,15 @@
+const cardUnlocked = () => {
+
+  const cards = document.querySelectorAll(`.store-card`)
+
+  cards.forEach((card) => {
+    if (card.querySelector('.store-progression-grey')) {
+      card.querySelector('.store-image').classList.add('store-card-locked');
+      card.querySelector('.store-card-detail').classList.add('store-card-locked');
+      console.log('test')
+    }
+  })
+
+};
+
+export { cardUnlocked };
