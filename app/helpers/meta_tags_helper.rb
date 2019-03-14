0facf481cedaf12fa259bb85def64658ddc0a8a4
Maximilian Wingbermuehle <max@texwork.com>
2019-03-14 13:59:52 +0100

    adds meta img

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -9,6 +9,7 @@ module MetaTagsHelper
 
   def meta_image
     meta_image = 'https://res.cloudinary.com/dmr1qubzk/image/upload/v1552568255/Screenshot_2019-03-14_at_13.49.54.png'
+
     # meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # # little twist to make it work equally with an asset or a url
     # meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
